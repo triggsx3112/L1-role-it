81d823490ef2657cf516119cc719ccba9dcc61cc
triggsx3112 <triggsx3112@masseyhigh.school.nz>
2025-02-05 11:59:34 +1300

    finished

--- C_01_yes_no_basic.py
+++ C_01_yes_no_basic.py
@@ -1,9 +1,17 @@
-want_instructions =input("Do you want to see the instructions? ").lower
 
-# check the user says yes/no
-if want_instructions == "yes" or want_instructions == "y":
-    print("You said yes")
-elif want_instructions == "no" or want_instructions == "n":
-    print("You said no")
-else:
-    print("please enter a yes / no")
+while True:
+
+    want_instructions = input("Do you want to see the instructions? ").lower()
+
+    # check the user says yes/no
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You said yes")
+        break
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You said no")
+        break
+    else:
+        print("please enter a yes / no")
+        continue
+
+print("We done")
